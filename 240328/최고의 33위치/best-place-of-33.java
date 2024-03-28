a6bdf89a854ec9e7a059d69a50fa25f7f8fb56f7
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-03-28 09:51:33 +0900

    Title: 최고의 33위치, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240328/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int maxOfCoin = 0;
+        int[][] xy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                xy[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i = 0; i<= n-3; i++){
+            for(int j =0; j<=n-3; j++){
+                maxOfCoin = Math.max(maxOfCoin, checkCoin(xy, i,j));
+            }
+        }
+        System.out.println(maxOfCoin);
+    }
+
+    public static int checkCoin(int[][] xy, int x, int y) {
+        int numOfCoin = 0;
+        for (int i = x; i <= x + 2; i++) {
+            for (int j = y; j <= y + 2; j++){
+                if(xy[i][j] == 1){
+                    numOfCoin++;
+                }
+            }
+        }
+        return numOfCoin;
+    }
+}
