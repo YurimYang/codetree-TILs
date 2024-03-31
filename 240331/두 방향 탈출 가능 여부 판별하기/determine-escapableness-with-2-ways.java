1a2142cbe7b8f350605fea111633f9d39788774c
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-03-31 11:31:29 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240331/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, m, ans=1;
+    static int[][] grid, visited;
+
+    public static void main(String args[]) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken()); //n*m 이차원 배열
+        grid = new int[n][n];
+        visited = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        DFS(0, 0);
+        if(visited[n-1][m-1] == 1){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+
+    public static void DFS(int x, int y) {
+        int[] dx = new int[]{1, 0};
+        int[] dy = new int[]{0, 1}; //아래, 오른쪽
+
+        for (int i = 0; i < 2; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if (canGo(newX, newY)){
+                visited[newX][newY] = 1;
+                DFS(newX,newY);
+            }
+        }
+    }
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
+    }
+
+    public static boolean canGo(int x, int y) {
+        if(!inRange(x,y)){
+            return false;
+        }
+        if(grid[x][y] == 0 || visited[x][y] == 1){
+            return false;
+        }
+        return true;
+    }
+}
