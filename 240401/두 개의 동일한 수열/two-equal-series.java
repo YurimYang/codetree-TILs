9dd5c9c4ed868f06a03c996344b1d4d806b80c08
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-01 12:17:13 +0900

    Title: 두 개의 동일한 수열, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240401/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+
+    public static void main(String args[]) throws IOException{
+        n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+        a = makeArray(a);
+        b = makeArray(b);
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int tmp = 0;
+        for(int i = 0; i<n ;i++){
+            if(a[i] == b[i]){
+                tmp++;
+            }else{
+                break;
+            }
+        }
+        if(tmp > 0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
+
+    public static int[] makeArray(int[] arr) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        return arr;
+    }
+}
