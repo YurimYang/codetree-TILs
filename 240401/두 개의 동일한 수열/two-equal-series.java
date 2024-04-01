c3b33c93be8ba25f09b56efae9e42d95a15365ca
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-01 12:25:37 +0900

    Title: 두 개의 동일한 수열, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240401/두 개의 동일한 수열/two-equal-series.java
+++ 240401/두 개의 동일한 수열/two-equal-series.java
@@ -5,30 +5,23 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int n;
+    static int[] a, b;
 
     public static void main(String args[]) throws IOException{
         n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
-        int[] b = new int[n];
+        a = new int[n];
+        b = new int[n];
         a = makeArray(a);
         b = makeArray(b);
 
         Arrays.sort(a);
         Arrays.sort(b);
 
-        int tmp = 0;
-        for(int i = 0; i<n ;i++){
-            if(a[i] == b[i]){
-                tmp++;
-            }else{
-                System.out.println("No");
-                break;
-            }
-        }
-        if(tmp == n){
+        if(isEqual()){
             System.out.println("Yes");
+        }else{
+            System.out.println("No");
         }
-
     }
 
     public static int[] makeArray(int[] arr) throws IOException {
@@ -37,5 +30,14 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         return arr;
+    }
+    
+    public static boolean isEqual(){
+        for(int i = 0; i<n ;i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
