b61b3a34fc0624f81f21d90e423bc8d3f26d526c
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-09 19:10:18 +0900

    Title: 병원 거리 최소화하기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/병원 거리 최소화하기/min-of-hospital-distance.java
+++ 240409/병원 거리 최소화하기/min-of-hospital-distance.java
@@ -10,6 +10,7 @@ public class Main {
     static int ans = Integer.MAX_VALUE;
     static int[][] grid, visited, step;
     static List<Point> hospital = new ArrayList<>();
+    static List<Point> people = new ArrayList<>();
     static Queue<Point> pickedHospital = new LinkedList<>();
     static boolean[] hospitalVisit = new boolean[13];
     
@@ -28,84 +29,38 @@ public class Main {
                 grid[i][j] = Integer.parseInt(st.nextToken());
                 if(grid[i][j] == 2){
                     hospital.add(new Point(i,j));
-                }
+                } else if(grid[i][j] == 1){
+                    people.add(new Point(i,j));
+                }  
             }
         }
         findM_hospital(0,-1);
         System.out.println(ans);
     }
 
-    public static void initial(){
-        for(int i = 0; i<n; i++){
-            for(int j = 0 ; j<n; j++){
-                step[i][j] = 0;
-                visited[i][j] = 0;
-            }
-        }
+    public static int getDistance(Point people, Point hospital){
+        return Math.abs(people.x - hospital.x) + Math.abs(people.y - hospital.y);
     }
 
-    public static void findPickedHospital(){
-        pickedHospital = new LinkedList<>();
-        for(int i = 0; i< hospital.size(); i++){
-            if(hospitalVisit[i]){
-                pickedHospital.add(hospital.get(i));
-            }
-        }
-    }
+    public static int getCurrMinDistance() {
+        int distance = 0;
 
-    public static int checkDistance(){
-        int total = 0;
-        int[] dx = new int[]{-1,1,0,0};
-        int[] dy = new int[]{0,0,-1,1};
-        initial();
-        findPickedHospital();
-
-        while(!pickedHospital.isEmpty()){
-            Point currHospital = pickedHospital.poll();
-            int x = currHospital.x; 
-            int y = currHospital.y;
-            visited[x][y] = 1;
-
-            for(int i = 0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(canGo(nx,ny)){
-                    pickedHospital.add(new Point(nx, ny));
-                    step[nx][ny] = step[x][y] + 1;
-                    visited[nx][ny] = 1;
+        for(int i = 0; i<people.size(); i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j<hospital.size(); j++){
+                if(hospitalVisit[j]){
+                    min = Math.min(min, getDistance(people.get(i), hospital.get(j)));
                 }
             }
+            distance += min;
         }
-
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(grid[i][j] == 1){
-                    total += step[i][j];
-                }
-            }
-        }
-        return total;
+        return distance;
     }
-
-    public static boolean inRange(int x, int y){
-        return 0<= x && x <n && 0<=y && y< n;
-    }
-
-    public static boolean canGo(int x, int y){
-        if(!inRange(x,y)){
-            return false;
-        }
-        if(visited[x][y] == 1 || grid[x][y] == 2){
-            return false;
-        }
-        return true;
-    } 
 
 
     public static void findM_hospital(int cnt, int latIdx){
         if(cnt == m){
-            ans = Math.min(ans, checkDistance());
+            ans = Math.min(ans, getCurrMinDistance());
             return;
         }
         for(int i = latIdx + 1; i < hospital.size(); i++){
