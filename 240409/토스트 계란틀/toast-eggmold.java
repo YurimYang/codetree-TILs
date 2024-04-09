185ac6f4664dbed21db6d0e3321ebbfcdf2aa120
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-09 10:03:23 +0900

    Title: 토스트 계란틀, Time: 559ms, Memory: 47MB, Status: Passed - CodeTree

--- 240409/토스트 계란틀/toast-eggmold.java
+++ 240409/토스트 계란틀/toast-eggmold.java
@@ -1,124 +1,141 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+class Pair {
+    int x, y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int n, L, R;
-    static int[][] grid, visited;
-    static Queue<Point> sum = new LinkedList<>();
-    static List<Point> group = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException{
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken()); //이상
-        R = Integer.parseInt(st.nextToken()); //이하
-        grid = new int[n][n];
-        visited = new int[n][n];
-        int ans = 0;
-
-        for(int i = 0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<n; j++){
-                grid[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        while(true){
-            if(!move()){
-                break;
-            }
-            ans++;
-        }
-        System.out.println(ans);
+    public static final int DIR_NUM = 4;
+    public static final int MAX_N = 50;
+    
+    public static int n, L, R;
+    
+    public static int[][] egg = new int[MAX_N][MAX_N];
+    
+    public static Queue<Pair> bfsQ = new LinkedList<>();
+    public static ArrayList<Pair> eggGroup = new ArrayList<>();
+    public static boolean[][] visited = new boolean[MAX_N][MAX_N];
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
     }
-
-    public static void mergeEggs(){
-        int total = 0;
-        for(Point point : group){
-            total += grid[point.x][point.y];
-        }
-        for(Point point : group){
-            grid[point.x][point.y] = total/group.size();
-        }
+    
+    public static boolean canGo(int x, int y, int currEgg) {
+        if(!inRange(x, y))
+          return false;
+          
+        int eggDiff = Math.abs(egg[x][y] - currEgg);
+        return !visited[x][y] 
+            && L <= eggDiff && eggDiff <= R;
     }
-
-    public static void BFS(){
-        int[] dx = new int[]{-1,1,0,0};
-        int[] dy = new int[]{0,0,-1,1};
-
-        while(!sum.isEmpty()){
-            Point currPoint = sum.poll();
-            int x = currPoint.x;
-            int y = currPoint.y;
-            
-            for(int i = 0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(canGo(nx, ny, x, y)){
-                    visited[nx][ny] = 1;
-                    group.add(new Point(nx, ny));
-                    sum.add(new Point(nx,ny));
+    
+    // visited 배열을 초기화 해줍니다.
+    public static void initializeVisited() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                visited[i][j] = false;
+    }
+    
+    public static void bfs() {
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+    
+        // BFS 탐색을 수행합니다.
+        while(!bfsQ.isEmpty()) {
+            Pair currPos = bfsQ.poll();
+            int currX = currPos.x, currY = currPos.y;
+    
+            for(int i = 0; i < DIR_NUM; i++) {
+                int newX = currX + dx[i];
+                int newY = currY + dy[i];
+    
+                // L, R 사이인 경우에만 합쳐질 수 있습니다.
+                if(canGo(newX, newY, egg[currX][currY])) {
+                    bfsQ.add(new Pair(newX, newY));
+                    eggGroup.add(new Pair(newX, newY));
+                    visited[newX][newY] = true;
                 }
             }
         }
     }
-
-    public static void initial(){
-        for(int i = 0; i< n; i++){
-            for(int j = 0; j<n; j++){
-                visited[i][j] = 0;
-            }
+    
+    // 계란들을 합칩니다.
+    public static void mergeEggs() {
+        int sumOfEggs = 0;
+        for(int k = 0; k < eggGroup.size(); k++) {
+            int x = eggGroup.get(k).x, y = eggGroup.get(k).y;
+            sumOfEggs += egg[x][y];
         }
+    
+        for(int k = 0; k < eggGroup.size(); k++) {
+            int x = eggGroup.get(k).x, y = eggGroup.get(k).y;
+            egg[x][y] = sumOfEggs / eggGroup.size();
+        }    
     }
-
-    public static boolean move(){
-        initial();
+    
+    // 조건에 맞게 계란의 양을 바꿔줍니다.
+    public static boolean moveEggs() {
+        // BFS 탐색을 위한 초기화 작업을 수행합니다.
+        initializeVisited();
+        
         boolean isChanged = false;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(visited[i][j] == 0){
-                    sum.add(new Point(i,j));
-                    group.add(new Point(i,j));
-                    visited[i][j] = 1;
-
-                    BFS();
-
-                    if(group.size() > 1){
+    
+        // 아직 방문하지 못한 칸에 대해
+        // BFS 탐색을 통해 합쳐질 계란들을 찾아냅니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j]){
+                    // 합쳐질 계란 목록을 담을 곳을 초기화합니다.
+                    eggGroup = new ArrayList<>();
+                    
+                    bfsQ.add(new Pair(i, j));
+                    eggGroup.add(new Pair(i, j));
+                    visited[i][j] = true;
+    
+                    bfs();
+                    
+                    // 계란의 이동이 한번이라도 일어났는지를 확인합니다.
+                    if(eggGroup.size() > 1)
                         isChanged = true;
-                    }
+                    
+                    // (i, j)와 관련이 있는 계란들을 합칩니다.
                     mergeEggs();
                 }
             }
         }
+        
         return isChanged;
-
     }
 
-    public static boolean canGo(int nx, int ny, int x, int y){
-        if(!inRange(nx,ny)){
-            return false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        L = sc.nextInt();
+        R = sc.nextInt();
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++) 
+                egg[i][j] = sc.nextInt();
+
+        int moveCnt = 0;
+        // 이동이 더 이상 필요 없을 때까지
+        // 계란의 이동을 반복합니다.
+        while(true) {
+            boolean isChanged = moveEggs();
+            if(!isChanged)
+                break;
+            
+            moveCnt++;
         }
-        int diff = Math.abs(grid[nx][ny] - grid[x][y]);
-        if(visited[nx][ny] == 1 || diff< L && diff > R){
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean inRange(int x, int y){
-        return 0<=x && x<n && 0<=y && y<n;
-    }
-}
-
-class Point{
-    int x;
-    int y;
-
-    public Point(int x, int y){
-        this.x = x;
-        this.y = y;
+        
+        System.out.print(moveCnt);
     }
 }
