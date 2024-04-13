107ca7e6983ee34b103dec3965ae608fe999ff08
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-13 10:04:56 +0900

    Title: 피보나치 수, Time: 3066ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

---
+++ 240413/피보나치 수/fibonacci-number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] memo;
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        memo = new int[n+1];
+        System.out.println(fibo(n));   
+    }
+
+    public static int fibo(int n){
+        if(n<=2){
+            memo[n] = 1;
+        } else {
+            memo[n] = fibo(n-1) + fibo(n-2);    
+        }
+        return memo[n];
+    }
+}
