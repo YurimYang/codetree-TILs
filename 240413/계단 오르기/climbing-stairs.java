779481a902a807f088883d3bc293268af9920fc8
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-13 10:29:20 +0900

    Title: 계단 오르기, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

--- 240413/계단 오르기/climbing-stairs.java
+++ 240413/계단 오르기/climbing-stairs.java
@@ -8,19 +8,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         stair = new double[1001];
-        stair[0] = 1;
-        stair[1] = 0;
-        System.out.println((int) (stairs(n) % 10007));
+        System.out.println((int) (stairs(n)));
     }
 
     public static double stairs(int n){
         if(stair[n] != 0){
             return stair[n];
         }
-        if(n == 2 || n == 3){
+        if(n == 0 || n == 2 || n == 3){
             stair[n] = 1;
+        } else if(n == 1){
+            stair[n] = 0;
         } else if (n > 3) {
-            stair[n] = stairs(n-2) + stairs(n-3);
+            stair[n] = (stairs(n-2) + stairs(n-3)) % 10007;
         }
         return stair[n];
     }
