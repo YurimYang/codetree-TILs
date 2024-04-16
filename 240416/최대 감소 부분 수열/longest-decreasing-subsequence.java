ec210a963ae95d6fb1f9dc199b70cd08728e78b8
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-16 19:59:35 +0900

    Title: 최대 감소 부분 수열, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240416/최대 감소 부분 수열/longest-decreasing-subsequence.java
+++ 240416/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -10,8 +10,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
         st= new StringTokenizer(br.readLine());
-        num = new int[n];
-        dp = new int[n];
+        num = new int[1000];
+        dp = new int[1000];
 
         for(int i = 0 ; i < n; i++){
             num[i] = Integer.parseInt(st.nextToken());
