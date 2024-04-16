06b7c98c5435c1ae8253a82be3d5da150888d124
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-16 14:53:50 +0900

    Title: 최대 증가 부분 수열, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240416/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] num;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException{
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        num = new int[n];
+        dp = new int[n];
+
+        for(int i = 0; i<n; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = num[0];
+
+        for(int i = 1; i<n; i++){
+            int max = Integer.MIN_VALUE;
+            int idx = 0;
+            for(int j = 0; j<i; j++){
+                if(num[j] < num[i]){
+                    max = Math.max(max, num[j]); 
+                    idx = j;
+                }  
+            }
+            dp[i] = dp[idx] + 1;
+        }
+
+        Arrays.sort(dp);
+        System.out.print(dp[n-1]);
+    }
+}
