3e6336eb8f59e3d2c3d5ba841a4dcee7e7d25b85
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-17 01:54:07 +0900

    Title: 최대 점프 횟수, Time: 115ms, Memory: 9MB, Status: Passed - CodeTree

--- 240417/최대 점프 횟수/maximum-number-of-jumps.java
+++ 240417/최대 점프 횟수/maximum-number-of-jumps.java
@@ -17,7 +17,7 @@ public class Main {
         }
 
         for(int i = 1; i<n; i++){
-            dp[i] = 0;
+            dp[i] = Integer.MIN_VALUE;
             for(int j = 0; j<i; j++){
                 if(i-j <= num[j]){
                     dp[i] = Math.max(dp[i], dp[j] + 1);
