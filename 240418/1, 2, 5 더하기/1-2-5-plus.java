43d21ea0f5ad319ffba3fe42da73886e69d1eab9
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-18 11:21:33 +0900

    Title: 1, 2, 5 더하기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240418/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+    static int[] plus = new int[]{1,2,5};
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException{
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[n+1];
+        dp[0] = 1;
+
+
+        for(int i = 1; i<n+1; i++){
+            for(int j = 0; j<3; j++){
+                if(i - plus[j] >=0 ){
+                    dp[i] += dp[i - plus[j]];
+                }
+            }
+        }
+
+        System.out.print(dp[n]);
+
+
+
+    }
+
+
+
+}
