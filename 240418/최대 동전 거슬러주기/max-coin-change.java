1974d1c7c65e6266c75ba95dfbdaa49a688b14f8
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-18 11:38:46 +0900

    Title: 최대 동전 거슬러주기, Time: 110ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240418/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st; 
+    static int n,m;
+    static int[] coin, dp;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        //동전 초기화
+        coin = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i<n; i++){
+            coin[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[m+1];
+        for(int i = 1; i<m+1; i++){
+            for(int j = 0; j<n; j++){
+                if(i-coin[j] >=0){
+                    dp[i] = Math.max(dp[i], dp[i-coin[j]]+1);
+                }
+            }
+        }
+
+        int ans = dp[m];
+        if(ans == 0){
+            System.out.print(-1);
+        }else{
+            System.out.print(ans);
+        }
+
+        
+    }
+}
