95bc6f1bba28b819f93d8a7045272df47cad82d9
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-18 11:23:52 +0900

    Title: 1, 2, 5 더하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240418/1, 2, 5 더하기/1-2-5-plus.java
+++ 240418/1, 2, 5 더하기/1-2-5-plus.java
@@ -18,7 +18,7 @@ public class Main {
         for(int i = 1; i<n+1; i++){
             for(int j = 0; j<3; j++){
                 if(i - plus[j] >=0 ){
-                    dp[i] += dp[i - plus[j]] % 10007;
+                    dp[i] = (dp[i] + dp[i - plus[j]]) % 10007;
                 }
             }
         }
