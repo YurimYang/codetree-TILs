fa6aa9f92fb7b31315bdd0dd9c524ce98672ba59
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-22 09:48:33 +0900

    Title: 이상한 진수 2, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

--- 240422/이상한 진수 2/awkward-digits-2.java
+++ 240422/이상한 진수 2/awkward-digits-2.java
@@ -25,10 +25,13 @@ public class Main {
             ans += Math.pow(2, changedNumber.length() - i - 1) * Integer.parseInt(String.valueOf(changedNumber.charAt(i)));
         }
 
+        //2진수 -> 10진수 (해답코드)
+        int num = 0;
+        for(int i = 0; i<changedNumber.length(); i++){
+            num = num * 2 + (changedNumber.charAt(i) - '0');
+        }
+
         System.out.print(ans);
-
-        
-
     }
 
     //0이 있을 경우
@@ -47,8 +50,4 @@ public class Main {
         number[number.length - 1] = '0';
         return String.valueOf(number);
     }
-
-    
-
-
 }
