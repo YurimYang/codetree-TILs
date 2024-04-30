9b30847adb27ae4d397104d7d5e18c6c15232354
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-30 14:14:46 +0900

    Title: 벌금은 누구에게, Time: 141ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240430/벌금은 누구에게/who-will-pay.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] person, calls;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        //사람 초기화
+        person = new int[n+1];
+
+        //호출번호 기록
+        int answer = -1;
+        for(int i = 0; i<m; i++){
+            int number = Integer.parseInt(br.readLine());
+            person[number]++;
+            for(int j = 1; j<n+1; j++){
+                if(person[j] >= k){
+                    answer = j;
+                    break;
+                }
+            }  
+        }
+        System.out.print(answer);  
+    }
+}
