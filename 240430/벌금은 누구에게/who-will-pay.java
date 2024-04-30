8e58d1710664acc4cea526c755097648cd19c7f1
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-30 14:17:14 +0900

    Title: 벌금은 누구에게, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

--- 240430/벌금은 누구에게/who-will-pay.java
+++ 240430/벌금은 누구에게/who-will-pay.java
@@ -17,15 +17,20 @@ public class Main {
 
         //호출번호 기록
         int answer = -1;
+        boolean isStop = false;
         for(int i = 0; i<m; i++){
             int number = Integer.parseInt(br.readLine());
             person[number]++;
             for(int j = 1; j<n+1; j++){
                 if(person[j] >= k){
                     answer = j;
+                    isStop = true;
                     break;
                 }
             }  
+            if(isStop){
+                break;
+            }
         }
         System.out.print(answer);  
     }
