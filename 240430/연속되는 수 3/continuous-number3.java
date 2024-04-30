6a2896769b07267af1b8e68ae920e574b839016e
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-04-30 11:36:39 +0900

    Title: 연속되는 수 3, Time: 102ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240430/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        
+        //numbers 초기화
+        numbers = new int[n];
+        for(int i = 0; i<n; i++){
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        //연속 부분 수열 구하기
+        int maxCnt = 0;
+        int duplicateCnt = 0;
+        for(int i = 0; i<n; i++){
+            if(i == 0 || (numbers[i] < 0 && numbers[i-1] < 0) 
+                ||  (numbers[i] > 0 && numbers[i-1] > 0)) {
+                    duplicateCnt++;
+            } else {
+                duplicateCnt = 1;
+            }
+            maxCnt = Math.max(maxCnt, duplicateCnt);
+        }
+        System.out.print(maxCnt);
+        
+    }
+}
