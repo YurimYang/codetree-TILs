58cf5fa4c7c69a086c33247275339fbbd30486b0
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-05-02 12:17:55 +0900

    Title: 선두를 지켜라, Time: 320ms, Memory: 33MB, Status: Passed - CodeTree

--- 240502/선두를 지켜라/keep-the-lead.java
+++ 240502/선두를 지켜라/keep-the-lead.java
@@ -16,21 +16,27 @@ public class Main {
         moveA = new int[MAX];
         moveB = new int[MAX];
         int[] movedA = movement(n, moveA);
+        // for(int i = 0 ;i<30; i++){
+        //     System.out.print(movedA[i] +" ");
+        // }
         int[] movedB = movement(m, moveB);
+        // System.out.println("----------------------------===============================================");
+        // for(int i = 0 ;i<30; i++){
+        //     System.out.print(movedB[i] +" ");
+        // }
 
         //선두 여부 확인
         System.out.println(isFaster(movedA, movedB));
     }
 
     public static int[] movement(int times, int[] move) throws IOException{
-        int start = 0;
+        int start = 1;
         for(int i = 0; i<times; i++){
             st = new StringTokenizer(br.readLine());
             int v = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
             for(int j = start; j<start + t; j++){
-                int distance = v * j;
-                move[j] = distance;
+                move[j] += move[j-1] +  v;  
             }
             start += t;
         }
@@ -41,19 +47,22 @@ public class Main {
         int min = Math.min(moveA.length, moveB.length);
         String fastFirst;
         int answer = 0;
-        if(moveA[0] > moveB[0]){
+        if(moveA[1] > moveB[1]){
             fastFirst = "A";
         } else {
            fastFirst = "B";
         }
+        //System.out.println(fastFirst);
         
-        for(int i = 0; i<min; i++){
+        for(int i = 1; i<min; i++){
             if(fastFirst.equals("A") && moveB[i] > moveA[i]){
                 fastFirst = "B";
                 answer++;
+                // System.out.println(i);
             } else if (fastFirst.equals("B") && moveB[i] < moveA[i]){
                 fastFirst = "A";
                 answer++;
+                // System.out.println(i);
             }
         }
         return answer;
