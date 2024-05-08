2f90507f1253994003de7fe20bedc51aa8a24aca
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-05-08 23:11:45 +0900

    Title: 동전 거슬러주기, Time: 125ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240508/동전 거슬러주기/coin-change.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, m;
+    static int[] coin, dp;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        coin = new int[n];
+        dp = new int[m+1];
+
+        st = new StringTokenizer(br.readLine());    
+        for(int i = 0; i<n; i++){
+            coin[i] = Integer.parseInt(st.nextToken());
+        }
+
+        initial();
+
+        for(int i = 1; i<m+1; i++){
+            for(int j = 0; j<n; j++){
+                if(i - coin[j] >= 0 && dp[i-coin[j]] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i - coin[j]] + 1);
+                }
+            }
+        }
+
+        int min = dp[m];
+
+        if(min == Integer.MAX_VALUE){
+            min = -1;
+        }
+
+        System.out.print(min);
+    }
+
+    public static void initial(){
+        for(int i = 1; i<m+1; i++){
+            dp[i] = Integer.MAX_VALUE;
+        } 
+
+        dp[0] = 0;
+    }
+}
