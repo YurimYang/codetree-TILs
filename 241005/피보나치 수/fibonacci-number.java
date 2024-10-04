1e19960345663c68a63071dfaa1e2be6e569d1e0
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-05 00:34:19 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241005/피보나치 수/fibonacci-number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        int n = Integer.parseInt(br.readLine());
+
+        if(n<=2){
+            System.out.println(1);
+        } else {
+            System.out.println(fibo(n));
+        }
+    }
+
+    public static int fibo(int n){
+        return fibo(n-1) + fibo(n-2);
+    }
+}
