1429344a63cdda0dd7175ea929976e91af0a4587
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-04 23:48:41 +0900

    Title: 그래프 탐색, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241004/그래프 탐색/graph-traversal.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, m, ans = 0;
+    static int[][] numbers;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        numbers = new int[n+1][n+1];
+        visited = new boolean[n+1];
+
+        for(int i = 0; i< m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            numbers[a][b] = 1;
+            numbers[b][a] = 1;
+        }
+
+        int currV = 1;
+        visited[currV] = true;
+        dfs(currV);
+        System.out.println(ans);
+    }
+
+
+    public static void dfs(int vertex){
+        for(int col = 1; col <=n; col++){
+            if(numbers[vertex][col] == 1 && !visited[col]){
+                System.out.println(vertex + " : " + col);
+                visited[vertex] = true;
+                ans++; 
+                dfs(col); 
+            }
+        }
+
+    }
+}
