61022699d6baced07275a6e5524cd87b980579bd
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-05 00:30:26 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241005/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, m = 0;
+    static int[][] graph;
+    static int[][] visited;
+    static Queue<Point> queue = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new int[n][m];
+        visited = new int[n][m];
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<m; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        queue.add(new Point(0,0));
+        visited[0][0] = 1;
+        bfs();
+        if(graph[n-1][m-1] == 1){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+    public static void bfs(){
+        int[] dx = {-1,1,0,0}; //상하좌우
+        int[] dy = {0,0,-1,1};
+        
+        while(!queue.isEmpty()){
+            Point curr = queue.poll();
+
+            for(int i = 0; i<4; i++){
+                int nx = curr.x + dx[i];
+                int ny = curr.y + dy[i];
+
+                if(canGo(nx, ny)){
+                    visited[nx][ny] = 1;
+                    queue.add(new Point(nx, ny));
+                }
+            }
+        }
+
+    }
+
+    public static boolean canGo(int x, int y){
+        if(!inRange(x, y)){
+            return false;
+        } else if(graph[x][y] == 0 || visited[x][y] != 0){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean inRange(int x, int y){
+        return 0<=x && x<n && 0<=y && y <m;
+    }
+}
+
+class Point{
+    int x; 
+    int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
