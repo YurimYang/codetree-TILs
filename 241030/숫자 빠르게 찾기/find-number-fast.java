c68352f05bba0702431b28aa71a742386ef35e15
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-30 14:26:26 +0900

    Title: 숫자 빠르게 찾기, Time: 1765ms, Memory: 55MB, Status: Passed - CodeTree

--- 241030/숫자 빠르게 찾기/find-number-fast.java
+++ 241030/숫자 빠르게 찾기/find-number-fast.java
@@ -1,44 +1,45 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        String[] nm = br.readLine().split(" ");
-        int n = Integer.parseInt(nm[0]);
-        int m = Integer.parseInt(nm[1]);
-
-        String[] numStr = br.readLine().split(" ");
-
-        List<Integer> numList = new ArrayList<>();
-
-        for(String num : numStr){
-            numList.add(Integer.parseInt(num));
+public class Main {    
+    public static final int MAX_N = 100000;
+    
+    public static int n, m;
+    public static int[] arr = new int[MAX_N];
+    
+    public static int find(int target) {
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] == target)
+                return mid;
+            
+            if(arr[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
+    
+        return -1;
+    }
 
-        for(int i = 0; i<m; i++){
-            int number = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-            long s = 0;
-            long e = n-1;
+        for(int i = 0; i < m; i++) {
+            int x = sc.nextInt();
 
-            long answer = -1;
-            while(s <= e){
-                long mid = (s+e) / 2;
-                if(number == numList.get((int) mid)){
-                    answer = mid + 1;
-                    break;
-                }
-                else if(number > numList.get((int) mid)){ 
-                    s = mid + 1;
-                } else {
-                    e = mid-1;
-                }
-            }
+            // 이진탐색을 진행합니다.
+            int index = find(x);
 
-            System.out.println(answer);
+            if(index >= 0)
+                System.out.println(index + 1);
+            else
+                System.out.println(-1);
         }
     }
 }
