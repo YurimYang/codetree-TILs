ed1080890b04cba83ce0a6b640f4e6f68cad69e8
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-30 16:23:40 +0900

    Title: 사각형 채우기 3, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241030/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+        
+    }
+}
